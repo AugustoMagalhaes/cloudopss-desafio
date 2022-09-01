aeebea93086a3d7ba0899d7dda72ea4ec18274a5
AugustoMagalhaes <augustommg@gmail.com>
2022-09-01 18:59:47 -0300

    feat: implementa OrangeBtn sem css com classe condicional.

---
+++ src/components/OrangeBtn/OrangeBtn.jsx
@@ -0,0 +1,21 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+function OrangeBtn({ hasBorder, text }) {
+  return (
+    <button type="button" className={`nav-btn ${hasBorder && 'whitebd'}`}>
+      {text}
+    </button>
+  );
+}
+
+OrangeBtn.propTypes = {
+  hasBorder: PropTypes.bool,
+  text: PropTypes.string.isRequired,
+};
+
+OrangeBtn.defaultProps = {
+  hasBorder: false,
+};
+
+export default OrangeBtn;
