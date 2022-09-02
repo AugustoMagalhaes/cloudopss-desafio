72b0a73e184f13c3afaa8b66f6e22d02d3dd1955
AugustoMagalhaes <augustommg@gmail.com>
2022-09-02 04:16:28 -0300

    refac: pequenas refatoraçoes de adequaçao ao figma.

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from '../components/Header/Header';
 import PersonalInfo from '../components/PersonalInfo/PersonalInfo';
 import Planning from '../components/Planning/Planning';
+import '../index.css';
 
 export default function MainPage() {
   return (
