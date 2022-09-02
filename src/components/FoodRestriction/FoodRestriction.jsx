300829fa8b40cbe14d3f47d508abe8ed2667b7e2
AugustoMagalhaes <augustommg@gmail.com>
2022-09-02 13:38:22 -0300

    feat: implementa componente FoodRestriction.

---
+++ src/components/FoodRestriction/FoodRestriction.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './FoodRestriction.css';
+
+function FoodRestriction() {
+  return (
+    <section className="foodrestriction-container">
+      <section className="foodrestriction-title-container">
+        <h3 className="foodrestriction-title">Você tem alguma</h3>
+        <h3 className="foodrestriction-title">restrição alimentar?</h3>
+      </section>
+      <section className="foodrestriction-input-container">
+        Input
+      </section>
+    </section>
+  );
+}
+export default FoodRestriction;
