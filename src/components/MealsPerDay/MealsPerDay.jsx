0b92d9588706b40a90035471fb6130c9c8232773
AugustoMagalhaes <augustommg@gmail.com>
2022-09-02 13:28:51 -0300

    feat: implementa componente MealsPerDay.

---
+++ src/components/MealsPerDay/MealsPerDay.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './MealsPerDay.css';
+
+function MealsPerDay() {
+  return (
+    <section className="mealsperday-container">
+      <section className="mealsperday-title-container">
+        <h3 className="mealsperday-title">Quantas refeições</h3>
+        <h3 className="mealsperday-title">você faz ao dia?</h3>
+      </section>
+      <section className="mealsperday-input-container">
+        Input
+      </section>
+    </section>
+  );
+}
+
+export default MealsPerDay;
