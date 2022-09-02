48d26e70a52dea2ec126cabea40f312a08a86b36
AugustoMagalhaes <augustommg@gmail.com>
2022-09-01 22:49:33 -0300

    feat: implementa section Planning.

---
+++ src/components/Planning/Planning.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import OrangeBtn from '../OrangeBtn/OrangeBtn';
+
+function Planning() {
+  return (
+    <section>
+      <h2 className="planning-title">
+        Planejamos tudo para você!
+      </h2>
+      <OrangeBtn text="Começar agora" />
+    </section>
+  );
+}
+
+export default Planning;
