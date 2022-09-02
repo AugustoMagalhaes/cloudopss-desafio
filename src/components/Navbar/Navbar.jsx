330a1297ea64d8dd08b5cd512c14813673e9b112
AugustoMagalhaes <augustommg@gmail.com>
2022-09-01 22:33:22 -0300

    chore: importa arquivos de css em Navbar.jsx e OrangeBtn.jsx.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import MinnaLogo from '../../images/minna-logo.png';
 import OrangeBtn from '../OrangeBtn/OrangeBtn';
+import './Navbar.css';
 
 function Navbar() {
   return (
-    <nav>
-      <section>
-        Minna
+    <nav className="nav-header">
+      <section className="logo-container">
+        <Link to="/">
+          <img src={MinnaLogo} alt="" />
+        </Link>
       </section>
-      <Link to="/">Como fazemos</Link>
-      <Link to="/">Planos e preços</Link>
-      <Link to="/">Receitas</Link>
-      <Link to="/">Sobre nós</Link>
-      <OrangeBtn hasBorder="" text="Inscreva-se" />
-      <OrangeBtn hasBorder="" text="Login" />
+      <Link to="/" className="nav-link">Como fazemos</Link>
+      <Link to="/" className="nav-link">Planos e preços</Link>
+      <Link to="/" className="nav-link">Receitas</Link>
+      <Link to="/" className="nav-link">Sobre nós</Link>
+      <OrangeBtn hasBorder text="Inscreva-se" />
+      <OrangeBtn hasBorder="" text="LOGIN" />
     </nav>
 
   );
