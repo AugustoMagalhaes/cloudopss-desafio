08ecbbe5746114dc2ae83be5d917829f4d645ae4
AugustoMagalhaes <augustommg@gmail.com>
2022-09-02 00:22:30 -0300

    chore: adequa estrutura de Planning.

--- src/components/Planning/Planning.jsx
+++ src/components/Planning/Planning.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import OrangeBtn from '../OrangeBtn/OrangeBtn';
+import './Planning.css';
 
 function Planning() {
   return (
-    <section>
-      <h2 className="planning-title">
-        Planejamos tudo para você!
-      </h2>
-      <OrangeBtn text="Começar agora" />
+    <section className="planning">
+      <section className="planning-title-container">
+        <h2 className="planning-title">
+          Planejamos
+        </h2>
+        <h2 className="planning-title">
+          tudo para você!
+        </h2>
+      </section>
+
+      <OrangeBtn specificClass="planning-btn" text="Começar agora" />
     </section>
   );
 }
