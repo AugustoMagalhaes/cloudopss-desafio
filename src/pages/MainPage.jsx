f22a9505cc433cbe64608f97f35436549ca18a72
AugustoMagalhaes <augustommg@gmail.com>
2022-09-02 00:23:19 -0300

    feat: implementa Planning em MainPage.

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import Header from '../components/Header/Header';
+import Planning from '../components/Planning/Planning';
 
 export default function MainPage() {
   return (
-    <Header />
+    <>
+      <Header />
+      <Planning />
+    </>
   );
 }
