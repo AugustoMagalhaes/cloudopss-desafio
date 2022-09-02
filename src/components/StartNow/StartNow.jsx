1ff395626bfd37d372c7494936afb9d88f0d8653
AugustoMagalhaes <augustommg@gmail.com>
2022-09-02 17:34:20 -0300

    feat: implementa componente StartNow.

---
+++ src/components/StartNow/StartNow.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './StartNow.css';
+
+function StartNow() {
+  return (
+    <section className="startnow-container">
+      <h2 className="startnow-title">Comece Agora!</h2>
+      <button type="submit" className="startnow-btn">
+        Clique aqui para ver sua dieta
+      </button>
+    </section>
+  );
+}
+
+export default StartNow;
