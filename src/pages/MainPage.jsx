19cdcc98fbb4c58dcaf46e6cc65b7f8b6eff7543
AugustoMagalhaes <augustommg@gmail.com>
2022-09-02 15:39:11 -0300

    feat: adiciona DietInvestment ao MainPage e troca imagem de chef-hat pra adequaçao a piggy-bank.

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import DietChoice from '../components/DietChoice/DietChoice';
 import DietDifficulty from '../components/DietDifficulty/DietDifficulty';
+import DietInvestment from '../components/DietInvestment/DietInvestment';
 import FoodRestriction from '../components/FoodRestriction/FoodRestriction';
 import Header from '../components/Header/Header';
 import MainGoal from '../components/MainGoal/MainGoal';
@@ -20,6 +21,7 @@ export default function MainPage() {
       <MealsPerDay />
       <FoodRestriction />
       <DietDifficulty />
+      <DietInvestment />
     </>
   );
 }
