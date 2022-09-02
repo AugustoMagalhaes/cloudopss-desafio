fec28da858255b87dda00eea1651a920558760ce
AugustoMagalhaes <augustommg@gmail.com>
2022-09-02 01:53:29 -0300

    chore: incorpora fonte Gilroy ao index e como padrao ao body do index.css.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './fonts/Gilroy/Radomir Tinkov - Gilroy-ExtraBold.otf';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
