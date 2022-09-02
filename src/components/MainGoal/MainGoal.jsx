563e36189775d3fef1b109340c65571c6516a279
AugustoMagalhaes <augustommg@gmail.com>
2022-09-02 04:42:15 -0300

    feat: implementa componente MainGoal.

---
+++ src/components/MainGoal/MainGoal.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './MainGoal.css';
+
+function MainGoal() {
+  return (
+    <section className="maingoal-container">
+      <section className="maingoal-title-container">
+        <h3 className="maingoal-title">
+          Qual é o seu
+        </h3>
+        <h3 className="maingoal-title">
+          objetivo principal?
+        </h3>
+      </section>
+      <section className="maingoal-input-container">
+        Inputs
+      </section>
+    </section>
+  );
+}
+
+export default MainGoal;
