3949a8e5ffa3b9fc75d4dda60f55054d6ec73e0c
AugustoMagalhaes <augustommg@gmail.com>
2022-09-02 23:55:19 -0300

    chore: checkpoint de Plans.

---
+++ src/components/PlanType/PlanType.jsx
@@ -0,0 +1,38 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import OrangeBtn from '../OrangeBtn/OrangeBtn';
+import './PlanType.css';
+
+function PlanType({ title, planPrice, planDescription }) {
+  return (
+    <section className="plan-type-container">
+      <section className="plan-type-title">
+        {title?.map((item, index) => (
+          <h3 className={index > 0 ? 'sub' : 'plan-title'}>
+            {item}
+          </h3>
+        ))}
+      </section>
+      <section className="main-content-plan">
+        <p className="real-sign">
+          R$
+        </p>
+        <p className="plan-type-price">
+          {planPrice}
+        </p>
+        <article className="plan-type-description">
+          {planDescription}
+        </article>
+        <OrangeBtn specificClass="plan-type-btn" text="Começar agora" />
+      </section>
+    </section>
+  );
+}
+
+PlanType.propTypes = {
+  title: PropTypes.arrayOf(PropTypes.string).isRequired,
+  planPrice: PropTypes.string.isRequired,
+  planDescription: PropTypes.string.isRequired,
+};
+
+export default PlanType;
