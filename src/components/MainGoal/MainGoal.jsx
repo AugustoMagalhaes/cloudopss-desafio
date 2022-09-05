ad872f292bccf81c2ee2be15d361a0c8770efa74
AugustoMagalhaes <augustommg@gmail.com>
2022-09-05 04:16:09 -0300

    chore: cria input select em MainGoal e estiliza de acordo com figma.

--- src/components/MainGoal/MainGoal.jsx
+++ src/components/MainGoal/MainGoal.jsx
@@ -13,7 +13,13 @@ function MainGoal() {
         </h3>
       </section>
       <section className="maingoal-input-container">
-        Inputs
+        <p className="arrow-down" />
+        <select name="goal" id="goal" size="4" className="select-goal">
+          <option value="weightloss" className="goal-option">Emagrecer</option>
+          <option value="keep" className="goal-option">Manter Peso</option>
+          <option value="health" className="goal-option">Ganhar saúde</option>
+          <option value="muscular" className="goal-option">Ganhar Massa Muscular</option>
+        </select>
       </section>
     </section>
   );
